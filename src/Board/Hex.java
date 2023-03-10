d750d80683e530d24e956139fd4aa8c5b2d4337d
SBleeJ <jenna.snow.lee@gmail.com>
2023-03-10 08:04:00 -0600

    pos1

--- src/Board/Hex.java
+++ src/Board/Hex.java
@@ -2,13 +2,18 @@ package Board;
 import java.util.*;
 public class Hex {
     int type;
-    Position position;
+    int x;
+    int y;
 
     public Hex(int type){
         this.type = type;
     }
     public int getType(){
         return type;
+    }
+    public void setXY(int x, int y){
+        this.x = x;
+        this.y = y;
     }
     
 }
